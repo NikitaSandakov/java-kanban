be386195346aa0921c88a4fe4532d1153c815102
Sandakov_Nikita <SandakovN71@gmail.com>
2025-01-19 06:06:01 +0500

    Код готов к проверке №1 1.2 Исправил вторую ошибку по CodeStyle при проверке пул реквеста

--- src/ru/scompany/trackerapp/model/Task.java
+++ src/ru/scompany/trackerapp/model/Task.java
@@ -8,7 +8,7 @@ public class Task {
     protected String description;
     protected TaskStatus status;
 
-    public Task (int id, String name, String description, TaskStatus status) {
+    public Task(int id, String name, String description, TaskStatus status) {
         this.id = id;
         this.name = name;
         this.description = description;
