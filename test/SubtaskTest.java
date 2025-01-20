ed3f07f3ad11ba0f794593afe45fde61b6ef948a
Sandakov_Nikita <SandakovN71@gmail.com>
2025-01-21 01:18:49 +0500

    Код готов к проверке №2 1. Убрал импорт через * 2. Использовал интерфейсы в InMemoryHistoryManager 3. Реализовал использование linkLast и getTasks в тестах 4. Использовал интерфейсы в InMemoryTaskManager

---
+++ test/SubtaskTest.java
@@ -0,0 +1,35 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import ru.scompany.trackerapp.model.Epic;
+import ru.scompany.trackerapp.model.Subtask;
+import ru.scompany.trackerapp.model.TaskStatus;
+import ru.scompany.trackerapp.service.InMemoryHistoryManager;
+import ru.scompany.trackerapp.service.InMemoryTaskManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SubtaskTest {
+
+    @Test
+    public void testSubtaskEquality() {
+        Subtask subtask1 = new Subtask(1, "Task 1", "Description 1", TaskStatus.NEW, 1);
+        Subtask subtask2 = new Subtask(1, "Task 1", "Description 1", TaskStatus.NEW, 1);
+
+        assertEquals(subtask1, subtask2);
+    }
+
+    @Test
+    public void testSubtaskCannotBeItsOwnEpic() {
+        InMemoryTaskManager taskManager = new InMemoryTaskManager(new InMemoryHistoryManager());
+
+        Epic epic = new Epic(1, "1 epic", "Description of epic");
+        taskManager.createEpic(epic);
+
+        Subtask subtask = new Subtask(1, "Subtask", "Description of subtask", TaskStatus.NEW, epic.getId());
+
+        assertNotEquals(epic, subtask);
+
+    }
+
+}
